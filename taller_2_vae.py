18358ce6179874e8c98889601019c414ab710deb
shernandez <sergio.hernandez@urjc.es>
2022-12-14 19:37:35 +0100

    utils data_adapter

--- taller_2_vae.py
+++ taller_2_vae.py
@@ -8,13 +8,51 @@ from tensorflow.keras.layers import Reshape, Conv2DTranspose
 from tensorflow.keras.layers import MaxPooling2D, UpSampling2D, Concatenate
 from tensorflow.keras import backend as K
 from tensorflow import keras
-from keras.engine import data_adapter
+# from keras.engine import data_adapter
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+def pack_x_y_sample_weight(x, y=None, sample_weight=None):
+  """Packs user-provided data into a tuple.
 
+  This is a convenience utility for packing data into the tuple formats
+  that `Model.fit` uses.
+
+  Standalone usage:
+
+  >>> x = tf.ones((10, 1))
+  >>> data = tf.keras.utils.pack_x_y_sample_weight(x)
+  >>> isinstance(data, tf.Tensor)
+  True
+  >>> y = tf.ones((10, 1))
+  >>> data = tf.keras.utils.pack_x_y_sample_weight(x, y)
+  >>> isinstance(data, tuple)
+  True
+  >>> x, y = data
+
+  Args:
+    x: Features to pass to `Model`.
+    y: Ground-truth targets to pass to `Model`.
+    sample_weight: Sample weight for each element.
+
+  Returns:
+    Tuple in the format used in `Model.fit`.
+  """
+  if y is None:
+    # For single x-input, we do no tuple wrapping since in this case
+    # there is no ambiguity. This also makes NumPy and Dataset
+    # consistent in that the user does not have to wrap their Dataset
+    # data in an unecessary tuple
+    if not tf.nest.is_nested(x):
+      return x
+    else:
+      return (x,)
+  elif sample_weight is None:
+    return (x, y)
+  else:
+    return (x, y, sample_weight)
 #
 data_path = "cityscapes"
 
